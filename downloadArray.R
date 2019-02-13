14a3f773ad728e41009d267916ccec6032ec14da
Petr Smirnov <psmirnov2000@gmail.com>
2019-02-13 14:52:24 -0500

    first

---
+++ downloadArray.R
@@ -0,0 +1,41 @@
+require(downloader)
+my.path="/pfs/out"
+my.path="/pfs/out"
+
+ftpdir <- "ftp://ftp.ebi.ac.uk//pub/databases/microarray/data/experiment/MTAB/E-MTAB-783/"
+myfn <- file.path(my.path, "celfile_timestamp.RData")
+
+message("Download genomic data")
+
+require(R.utils) || stop("Library R.utils is not available!")
+
+dir.create(file.path(my.path, "dwl"), showWarnings=FALSE, recursive=TRUE)
+
+## download and compress CEL files
+celfile.timestamp <- celfn <- NULL
+i <- 1
+while(i <= 9) {
+## assuming there are only 9 zip archives (need to check if the update version has more)
+dwl.status <- download.file(url=sprintf("%s/E-MTAB-783.raw.%i.zip", ftpdir, i), destfile=file.path(my.path, "dwl", sprintf("E-MTAB-783.raw.%i.zip", i)), quiet=TRUE)
+	if(dwl.status != 0) {
+		message("\t-> download failed, let's try again ...")
+		file.remove(file.path(my.path, "dwl", sprintf("E-MTAB-783.raw.%i.zip", i)))
+		i <- i - 1
+		} else {
+	## unzip archive
+	fff <- unzip(zipfile=file.path(my.path, "dwl", sprintf("E-MTAB-783.raw.%i.zip", i)), list=TRUE)
+	celfile.timestamp <- c(celfile.timestamp, as.character(fff[ ,"Date"]))
+	celfn <- c(celfn, as.character(fff[ ,"Name"]))
+	res <- unzip(zipfile=file.path(my.path, "dwl", sprintf("E-MTAB-783.raw.%i.zip", i)), exdir=my.path)
+	## compress each CEL file individually using gzip
+	library(R.utils)
+	sapply(file.path(my.path, as.character(fff[ ,"Name"])), R.utils::gzip, overwrite=TRUE)
+	i <- i + 1
+	}
+}
+celfile.timestamp <- t(sapply(strsplit(celfile.timestamp, split=" "), function(x) { return(x) }))
+dimnames(celfile.timestamp) <- list(celfn, c("file.day", "file.hour"))
+
+# unlink(file.path(my.path, "dwl"), recursive=TRUE)
+write.csv(celfile.timestamp, file=file.path(my.path, "celfile_timestamp.csv"))
+
